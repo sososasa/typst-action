2cdea7d9d92b357e6cdaf20e3cca142775a3fcf3
Saman Ray <samanshrestharay@gmail.com>
2025-08-09 15:33:50 -0400

    Update entrypoint script to accept source file as an argument and improve path handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,25 @@
 set -e
 
 # The first argument is the source file, which is required.
-SOURCE_FILE="main.typ"
+SOURCE_FILE=$1
 
 # The second argument is the optional output file.
 OUTPUT_FILE=$2
 
+# Check if a source file was provided
+if [ -z "$SOURCE_FILE" ]; then
+  echo "Error: No source file provided"
+  exit 1
+fi
+
+cd /github/workspace
+
 # Check if an output file was provided.
 if [ -n "$OUTPUT_FILE" ]; then
   # If an output file is specified, include it in the command.
-  typst compile "$SOURCE_FILE" "$OUTPUT_FILE"
+  typst compile "./$SOURCE_FILE" "$OUTPUT_FILE"
 else
   # If no output file is specified, just compile the source.
   # Typst will automatically name the output PDF based on the source file name.
-  typst compile "$SOURCE_FILE"
+  typst compile "./$SOURCE_FILE"
 fi
