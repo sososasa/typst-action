f0e28066deeeffc6364eb1105d7f8432f41f1641
Saman Ray <samanshrestharay@gmail.com>
2025-08-09 15:37:18 -0400

    Refactor entrypoint script to improve variable assignment and echo statements for clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@
 set -e
 
 # The first argument is the source file, which is required.
-SOURCE_FILE=$1
-
 # The second argument is the optional output file.
+SOURCE_FILE=$1
 OUTPUT_FILE=$2
 
 # Check if a source file was provided
@@ -15,14 +14,17 @@ if [ -z "$SOURCE_FILE" ]; then
   exit 1
 fi
 
+# Change to github workspace
 cd /github/workspace
 
 # Check if an output file was provided.
 if [ -n "$OUTPUT_FILE" ]; then
   # If an output file is specified, include it in the command.
+  echo "Compiling $SOURCE_FILE to $OUTPUT_FILE"
   typst compile "./$SOURCE_FILE" "$OUTPUT_FILE"
 else
   # If no output file is specified, just compile the source.
   # Typst will automatically name the output PDF based on the source file name.
+  echo "Compiling $SOURCE_FILE"
   typst compile "./$SOURCE_FILE"
 fi
